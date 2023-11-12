2014c967548aa9fbeaf3bc295551c42dd06714bc
TH-Raju <rajukhan894200@gmail.com>
2023-11-12 20:56:14 +0600

    function

--- module-2/src/functions.ts
+++ module-2/src/functions.ts
@@ -14,3 +14,16 @@ const addArrow = (n1: number, n2: number): number => {
 const arr = [1, 2, 3, 4, 5];
 
 const newArr = arr.map((ar: number) => ar * ar);
+
+// function in object
+const person: {
+  name: string;
+  balance: number;
+  addBalance(m: number): void;
+} = {
+  name: "Raju",
+  balance: 5,
+  addBalance(m: number) {
+    console.log(`my new balance ${this.balance + m}`);
+  },
+};
